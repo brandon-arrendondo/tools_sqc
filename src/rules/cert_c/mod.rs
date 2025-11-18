368c8f08945a64c9bc379462cf742e53114c9a02
Tristan VanFossen <tristan.vanfossen@bissell.com>
2025-11-18 08:46:22 -0500

    P2-FIO42-C: Implementation complete
    - Implemented FIO42-C rule: Close files when they are no longer needed
    - Detects unclosed FILE* from fopen/freopen
    - Detects unclosed POSIX file descriptors from open()
    - Detects unclosed Windows HANDLEs from CreateFile()
    - Tracks resources across function scope
    - All 8 tests passing (4 fail + 4 pass)

--- src/rules/cert_c/mod.rs
+++ src/rules/cert_c/mod.rs
@@ -103,6 +103,9 @@ pub mod fio18_c;
 #[path = "FIO/FIO20-C/fio20_c.rs"]
 pub mod fio20_c;
 
+#[path = "FIO/FIO42-C/fio42_c.rs"]
+pub mod fio42_c;
+
 #[path = "FIO/FIO37-C/fio37_c.rs"]
 pub mod fio37_c;
 
@@ -217,6 +220,7 @@ impl RuleRegistry {
         registry.register(Box::new(fio10_c::Fio10C));
         registry.register(Box::new(fio18_c::Fio18C));
         registry.register(Box::new(fio20_c::Fio20C));
+        registry.register(Box::new(fio42_c::Fio42C));
         registry.register(Box::new(fio37_c::Fio37C));
         registry.register(Box::new(flp30_c::Flp30C));
         registry.register(Box::new(int01_c::Int01C));
