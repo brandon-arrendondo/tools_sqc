3da51dccd4635147048bc2510e808d720e4532ea
Tristan VanFossen <tristan.vanfossen@bissell.com>
2025-12-01 13:42:48 -0500

    P2-EXP12-C: Add missing registry registration (100% pass rate)
    - Added exp12_c::Exp12C to registry.register() in mod.rs
    - Module was already declared but not registered
    - All 2 tests now passing (100%)

--- src/rules/cert_c/mod.rs
+++ src/rules/cert_c/mod.rs
@@ -723,6 +723,7 @@ impl RuleRegistry {
         registry.register(Box::new(exp36_c::Exp36C));
         registry.register(Box::new(exp10_c::Exp10C));
         registry.register(Box::new(exp11_c::Exp11C));
+        registry.register(Box::new(exp12_c::Exp12C));
         registry.register(Box::new(exp40_c::Exp40C));
         registry.register(Box::new(exp42_c::Exp42C));
         registry.register(Box::new(exp43_c::Exp43C));
