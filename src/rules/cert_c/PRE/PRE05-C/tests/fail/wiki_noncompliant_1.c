556a1752963edabcaafe58fb9b9074e722d70103
Tristan VanFossen <tristan.vanfossen@bissell.com>
2025-12-03 14:28:02 -0500

    P2-PRE11-C, P2-PRE05-C: Fix test files and implementation (100%)
    PRE11-C: Remove corrupted test file wiki_noncompliant_2_2.c
    PRE05-C: Fix test file and improve wrapper detection logic

--- src/rules/cert_c/PRE/PRE05-C/tests/fail/wiki_noncompliant_1.c
+++ src/rules/cert_c/PRE/PRE05-C/tests/fail/wiki_noncompliant_1.c
@@ -2,7 +2,12 @@
  * Rule: PRE05-C
  * Source: wiki
  * Status: FAIL - Should trigger PRE05-C violation
+ *
+ * Non-compliant: JOIN uses ## directly, so __LINE__ is not expanded
+ * before concatenation, resulting in "assertion_failed_at_line___LINE__"
  */
+
+#define JOIN(x, y) x ## y
 
 #define static_assert(e) \
   typedef char JOIN(assertion_failed_at_line_, __LINE__) \
