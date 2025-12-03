c7f0081264cbf341e4b136f64aa1288ac0a62c7e
Tristan VanFossen <tristan.vanfossen@bissell.com>
2025-12-03 13:39:18 -0500

    P2-PRE01-C: Fix incomplete test files (100%)
    Test files were missing macro definitions needed for detection:
    - wiki_noncompliant_2_2.c: Added CUBE macro
    - wiki_noncompliant_3_3.c: Added SQUARE macro
    
    All 4 tests pass. Move proposal to STAGED.

--- src/rules/cert_c/PRE/PRE01-C/tests/fail/wiki_noncompliant_2_2.c
+++ src/rules/cert_c/PRE/PRE01-C/tests/fail/wiki_noncompliant_2_2.c
@@ -2,6 +2,10 @@
  * Rule: PRE01-C
  * Source: wiki
  * Status: FAIL - Should trigger PRE01-C violation
+ *
+ * Non-compliant macro with unparenthesized parameters.
+ * CUBE(2 + 1) expands to (2 + 1 * 2 + 1 * 2 + 1) = 11 instead of 27
  */
 
+#define CUBE(I) (I * I * I)
 int a = 81 / CUBE(2 + 1);
